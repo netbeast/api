1456a5b04f13c8e40c69da81c7ce61e060289c73
jsdario <jesus@netbeast.co>
2016-03-31 11:51:17 +0200

    refactor

--- samples/loop.js
+++ samples/loop.js
@@ -1,8 +1,8 @@
-var resources = require('../lib/resources')
+var netbeast = require('..')
 
 var color = 0
 setInterval(function () {
   color += 2000
   var colorHue = Math.floor(color % 360)
-  resources('lights').set({power: 1, brightness: 100, hue: colorHue, saturation: 100})
+  netbeast('lights').set({power: 1, brightness: 100, hue: colorHue, saturation: 100})
 }, 1000)
